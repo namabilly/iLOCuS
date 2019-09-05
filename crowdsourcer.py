493ff60ffe19d974d7a5417021daf1baeee3165a
0Accelerator0 <948386361@qq.com>
2019-09-05 16:18:37 +0800

    Added basic structure

---
+++ crowdsourcer.py
@@ -0,0 +1,22 @@
+
+import model
+
+# take in current distribution with agent,
+# desired distribution,
+# budget limit
+
+class CrowdSourcer:
+	
+	def __init__(self):
+		
+		
+	# give out orders
+	def incentivitize(self, distribution, budget):
+		# strategy
+		
+	# compare and eval 2 distributions in some way	
+	def eval_distribution(self, distribution):
+	
+	
+	
+	
--- model.py
+++ model.py
@@ -1,10 +1,8 @@
 
-
+# Model - the simulation
+# not to be confused with prediction
 
 class Model:
-
-	p_accept = 0.8 # prob of agents accepting incentive
-	# can move it to agent for specific prob
 	
 	# size of the map, might need some casting for position from GPS
 	# num of agents, 
@@ -17,6 +15,21 @@ class Model:
 		self.agents[id] = Agent(position, status, sensor)
 		self.num_of_agents += 1
 	
+	# return prob of agents accepting incentive
+	def p_agent_accept(self, id, target, incentive):
+		pos = self.agents[id].position
+		# time, path = min(self.get_trajactories(pos, target))
+		# return (incentive - threshold) / (time * k)
+		
+	# simulate how the agents react to the incentive orders
+	def react_agents(self, orders):
+		# note that the order should be optimized through crowdsourcer,
+		# may give same location to multiple agents (?)
+		# should one agent receive more than one?
+		# for order in orders:
+		#	determine what each agent does
+		# self.update()
+		
 	def get_distribution(self):
 		''' pseudo
 		distribution = []
@@ -24,18 +37,24 @@ class Model:
 			distribution[pos]
 			count the sensors
 		return distribution
+		or return map
 		'''
 		
 	# should take traffic into account, posbly use gps api
 	def get_trajectories(self, source, target):
+		# simulation
+		# Maybe we need another class to generate these data
+		# get a map of routes and num of cars
 		return []
 		
 	# assume json
 	def update(self, data):
+		self.map = []
 		for ag in data.agents:
 			self.agents[ag.id].update(ag.position, ag.status)
 			# may cast position
 			self.map[ag.position].append(id)
+
 
 class Agent:
 
@@ -53,6 +72,6 @@ class Agent:
 		self.position = position
 		self.status = status
 		
-		
+	
 	
 	
