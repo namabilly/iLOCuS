d07778cca590661e3f5db1ee5c6a4ffbba4acfe2
0Accelerator0 <948386361@qq.com>
2019-10-25 21:53:50 -0700

    Added functions for random normalization for passenger pickup location matrix and driving routes; added functions to extract information of pickup information and routes

--- main.py
+++ main.py
@@ -8,23 +8,33 @@ from view import *
 data = np.load('20151101.npy')
 
 model = Model()
-view = View(model)
+# view = View(model)
 
 # time utc 1101 - 1446336000
 # 8 hours diff - 1446307200
 
 currtime = 1446307200
+# store info where vehicles pick up passengers
+filteredarray = []
 while currtime < 1446393600:
 	# currdata = filter(lambda x: currtime <= x[1] < currtime + 60, data)
 	currdata = data[np.logical_and(currtime <= data[:, 1], data[:, 1] < currtime + 120)]
 	# print(currdata)
+	
+	# matrix = [[0 for x in range(10)] for y in range(10)] 
+	statearray = []
+	
 	for d in currdata:
-		model.add_agent(d[0], (d[2], d[3]), d[6], None)
-	# print(model.agents)
-	view.draw()
+		model.add_agent(d[0], d[1], (d[2], d[3]), d[6], None, statearray)
+	
+	# print(currtime, statearray)
+	# load filtered data
+	for array in statearray:
+		filteredarray.append(array)
+	# view.draw()
 	currtime += 120
-	time.sleep(0.01)
+	# time.sleep(0.01)
 
-
-
+filteredarray = np.array(filteredarray, dtype='float')
+np.save('20151101_filtered.npy', filteredarray)
 
--- model.py
+++ model.py
@@ -1,4 +1,6 @@
-
+import math
+import numpy as np
+import time
 # Model - the simulation
 # not to be confused with prediction
 
@@ -10,10 +12,46 @@ class Model:
 		self.agents = {}
 		self.num_of_agents = 0
 		self.map = []
+		self.prediction_pos = {}
 	
-	def add_agent(self, id, position, status, sensor):
+	def add_agent(self, id, time, position, status, sensor, matrix):
+		# status change
+		id = int(id)
+		if id in self.agents:
+			if self.agents[id].status == 1 and status == 0:
+				if matrix != None:
+					# x, y = self.map_coordinate(position, [[39.60, 40.25], [116.04, 116.88]], [len(matrix), len(matrix[0])])
+					# matrix[x][y] += 1
+					matrix.append([id, time, position[0], position[1], status])					
 		self.agents[id] = Agent(position, status, sensor)
 		self.num_of_agents += 1
+	
+	def map_coordinate(self, position, range, slice):
+		incre_x = (range[0][1] - range[0][0])/slice[0]
+		incre_y = (range[1][1] - range[1][0])/slice[1]
+		x = math.floor((position[0] - range[0][0])/incre_x)
+		y = math.floor((position[1] - range[1][0])/incre_y)
+		return (x, y)
+		
+	# give random normalized matrix
+	def normalize_matrix(self, matrix, sigma):
+		np.random.seed(math.floor(time.time()))
+		for x in range(len(matrix)):
+			for y in range(len(matrix[0])):
+				matrix[x][y] = np.random.normal(matrix[x][y], sigma, None)
+				# matrix[x][y] = math.ceiling(matrix[x][y])
+				if matrix[x][y] < 0:
+					matrix[x][y] = 0
+		return matrix
+	
+	# give random normalized route positions
+	def normalize_route(self, routes, sigma):
+		# sigma recommended to be fairly small due to logitude/latitude
+		np.random.seed(math.floor(time.time()))
+		for x in range(len(routes)):
+			routes[x][0] = np.random.normal(routes[x][0], sigma, None)
+			routes[x][1] = np.random.normal(routes[x][1], sigma, None)
+		return routes
 	
 	# return prob of agents accepting incentive
 	def p_agent_accept(self, id, target, incentive):
@@ -62,6 +100,7 @@ class Agent:
 
 	def __init__(self, position, status, sensor):
 		self.position = position
+		self.starting_pos = position
 		self.status = status
 		'''
 		0 - free
@@ -76,10 +115,16 @@ class Agent:
 		
 	# visualization
 	def draw(self, canvas):
+		# whole Beijing
 		# 39.44 - 41.06
 		# 115.42 - 117.50
-		x = (self.position[1] - 115.42) * 500 / 2.08
-		y = (self.position[0] - 39.44) * 500 / 1.62
+		
+		# in this section as of 20151101
+		# 39.60 - 40.25
+		# 116.04 - 116.88
+		
+		x = (self.position[1] - 116.04) * 500 / .84
+		y = (self.position[0] - 39.60) * 500 / .65
 		color = 'green' if self.status == 0 else 'pink'
 		point = canvas.create_oval(x, y, x+1, y+1, outline=color, fill=color)
 	
