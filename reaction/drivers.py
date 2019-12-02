9b8732fcbab18f11680343cd8523b69d0f9fa664
football1222 <liuhanche@outlook.com>
2019-12-01 21:41:13 -0800

    Read real request data into driver reaction

--- reaction/drivers.py
+++ reaction/drivers.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import os
+import pickle
 
 class Driver:    
     def __init__(self, x, y):
@@ -17,61 +18,59 @@ class Drivers:
     LAT_GRID = 15
     LNG_STEP = (LNG_MAX - LNG_MIN) / LNG_GRID
     LAT_STEP = (LAT_MAX - LAT_MIN) / LAT_GRID
-    CNT_GRID = LNG_GRID * LAT_GRID
-    PRICE_PER_KM = 0.60 # gas or overall?
-    CIRCUITY_FACTOR = 1.34 # 1.2 - 1.6
     MOVE = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
     def __init__(self):
         self.time_idx = 0
         self.drivers = []
+        self.requests = {}
 
-    def reset(self, time = 9, count = 2000, date = "20151101"):
+    def reset(self, time = 9, count = 1000, date = "20151101"):
         random.seed(None)
         self.time_idx = time * 30
         self.drivers = []
         for i in range(count):
             # TODO: initial position needed
             self.drivers.append(Driver(random.randrange(self.LAT_GRID), random.randrange(self.LNG_GRID)))
-        
-        # dir_path = os.path.join(os.path.dirname(__file__), "../matrices_10min")
-        # req_path = os.path.join(dir_path, date + "_request.npy")
-        # self.ride_requests = np.load(req_path)
+
+        dir_path = os.path.join(os.path.dirname(__file__), "../requests_2min")
+        req_path = os.path.join(dir_path, date + "_request_list.pickle")
+        with open(req_path, 'rb') as f:
+            self.requests = pickle.load(f)
         return self.state()
 
     def inside(self, x, y):
         return x >= 0 and x < self.LAT_GRID and y >= 0 and y < self.LNG_GRID
 
-    def load_requests(self, time):
-        # TODO: fetch for this timestep
-        generated = [[[] for i in range(self.LAT_GRID)] for j in range(self.LNG_GRID)]
-        for i in range(self.LAT_GRID):
-            for j in range(self.LNG_GRID):
-                for k in range(random.randrange(5)): # number of requests in a grid
-                    start = (random.randrange(self.LAT_GRID), random.randrange(self.LNG_GRID))
-                    request = [start]
-                    for l in range(random.randrange(10)): # length of trip
-                        lst = request[-1]
-                        mov = self.MOVE[random.randrange(4)]
-                        nxt = (lst[0] + mov[0], lst[1] + mov[1])
-                        if self.inside(nxt[0], nxt[1]):
-                            request.append(nxt)
-                        else:
-                            request.append(lst)
-                    generated[i][j].append(request)
-        return generated
+    def load_requests(self):
+        # TODO: unmatched requests prosponed?
+        return self.requests[self.time_idx]
+        # generated = [[[] for i in range(self.LAT_GRID)] for j in range(self.LNG_GRID)]
+        # for i in range(self.LAT_GRID):
+        #     for j in range(self.LNG_GRID):
+        #         for k in range(random.randrange(5)): # number of requests in a grid
+        #             start = (random.randrange(self.LAT_GRID), random.randrange(self.LNG_GRID))
+        #             request = [start]
+        #             for l in range(random.randrange(10)): # length of trip
+        #                 lst = request[-1]
+        #                 mov = self.MOVE[random.randrange(4)]
+        #                 nxt = (lst[0] + mov[0], lst[1] + mov[1])
+        #                 if self.inside(nxt[0], nxt[1]):
+        #                     request.append(nxt)
+        #                 else:
+        #                     request.append(lst)
+        #             generated[i][j].append(request)
+        # return generated
 
 
-    def state(self, requests = None):
+    def state(self):
         taxi_count = np.zeros((self.LAT_GRID, self.LNG_GRID))
         empty_count = np.zeros((self.LAT_GRID, self.LNG_GRID))
         request_count = np.zeros((self.LAT_GRID, self.LNG_GRID))
         for driver in self.drivers:                   
             taxi_count[driver.grid_x][driver.grid_y] += 1
-            if requests is not None:
-                request_count[driver.grid_x][driver.grid_y] = len(requests[driver.grid_x][driver.grid_y])
+            request_count[driver.grid_x][driver.grid_y] = len(self.requests[self.time_idx][driver.grid_x][driver.grid_y])
             if not driver.itinerary: 
-                # not occupied (add for lottery)
                 empty_count[driver.grid_x][driver.grid_y] += 1
                 
         ret = np.zeros((4, self.LAT_GRID, self.LNG_GRID))
@@ -93,11 +92,11 @@ class Drivers:
                 taxis[driver.grid_x][driver.grid_y].append(driver)
 
         # lottery pick to match requests & drivers
-        requests = self.load_requests(self.time_idx)
+        requests = self.load_requests()
         for i in range(self.LAT_GRID):
             for j in range(self.LNG_GRID):
                 if len(requests[i][j]) > 0 and len(taxis[i][j]) > 0:
-                    for request in requests[i][j]:
+                    for key, request in requests[i][j].items():
                         if len(taxis[i][j]) == 0:
                             break
                         random.shuffle(taxis[i][j])
@@ -116,29 +115,24 @@ class Drivers:
                         cx, cy = nx, ny
                 driver.grid_x, driver.grid_y = cx, cy
             else:
-                # currently occupied
+                # currently occupied: go to the next grid in itinerary
                 # for starting rides, driver will stay in the current grid (time cost of take request)
                 # for finishing rides, driver will become available in next step (time cost of dropping)
                 next_grid = driver.itinerary.pop(0)
                 driver.grid_x, driver.grid_y = next_grid[0], next_grid[1]
 
         # compute new state for output
-        return self.state(requests)
+        return self.state()
                         
-
-    
+   
 
 # test its functionality
 # [720 * [15 * 15]]
 # data = np.load('../dataset/20151101_request.npy')
-# for i in data:
-#     print(i)
-#     break
 # print(len(data))
-# print(data[300])
 # print(data[360])
 drivers = Drivers()
-drivers.reset(1, 2000)
-#drivers.load_requests(1212)
+drivers.reset(9, 1000)
 drivers.step(np.ones((15,15)))
 drivers.step(np.zeros((15,15)))
+
