da9c7d42accf0f6e7514b5dee838715ac8ca193c
Suesoso <xssthu@gmail.com>
2020-05-11 21:16:12 -0700

    update and fix bugs

--- RL_main.py
+++ RL_main.py
@@ -19,6 +19,40 @@ from RL.objectives import mean_huber_loss
 from keras.optimizers import Adam, SGD
 from keras.callbacks import LearningRateScheduler
 
+
+def get_output_folder(parent_dir, env_name):
+    """Return save folder.
+    Assumes folders in the parent_dir have suffix -run{run
+    number}. Finds the highest run number and sets the output folder
+    to that number + 1. This is just convenient so that if you run the
+    same script multiple times tensorboard can plot all of the results
+    on the same plots with different names.
+    Parameters
+    ----------
+    parent_dir: str
+      Path of the directory containing all experiment runs.
+    Returns
+    -------
+    parent_dir/run_dir
+      Path to this run's save directory.
+    """
+    os.makedirs(parent_dir, exist_ok=True)
+    experiment_id = 0
+    for folder_name in os.listdir(parent_dir):
+        if not os.path.isdir(os.path.join(parent_dir, folder_name)):
+            continue
+        try:
+            folder_name = int(folder_name.split('-run')[-1])
+            if folder_name > experiment_id:
+                experiment_id = folder_name
+        except:
+            pass
+    experiment_id += 1
+
+    parent_dir = os.path.join(parent_dir, env_name)
+    parent_dir = parent_dir + '-run{}'.format(experiment_id)
+    return parent_dir
+
 def main():  # noqa: D103
     parser = argparse.ArgumentParser(description="Run DQN on iLOCuS")
     parser.add_argument("--network_name", default="deep_q_network", type=str, help="Type of model to use")
@@ -31,18 +65,19 @@ def main():  # noqa: D103
     parser.add_argument("--epsilon", default=0.5, type=float, help="Exploration probability for epsilon-greedy")
     parser.add_argument("--target_update_freq", default=10000, type=int,
                         help="Frequency for copying weights to target network")
-    parser.add_argument("--num_iterations", default=5000000, type=int,
+    parser.add_argument("--num_iterations", default=1000, type=int,
                         help="Number of overal interactions to the environment")
 
     parser.add_argument("--save_interval", default=2000, type=int, help="Interval of saving model")
-    parser.add_argument("--max_episode_length", default=400, type=int, help="Terminate earlier for one episode")
+    parser.add_argument("--max_episode_length", default=20, type=int, help="Terminate earlier for one episode")
     parser.add_argument("--train_freq", default=8, type=int, help="Frequency for training")
     parser.add_argument("--train_interv", default=8, type=int, help="interval for training")
     parser.add_argument("--num-burn-in", default=1000, type=int, help="number of memory before train")
 
     parser.add_argument("-o", "--output", default="ilocus-v0", type=str, help="Directory to save data to")
+    parser.add_argument('--env', default='driverSim-v0', help='Driver simulator env name')
     parser.add_argument("--seed", default=0, type=int, help="Random seed")
-    parser.add_argument("--train", default=True, type=bool, help="Train/Evaluate, set True if train the model")
+    parser.add_argument("--train", default=False, action='store_true', help="Train/Evaluate, set True if train the model")
     parser.add_argument("--model_path", default="atari-v0", type=str, help="specify model path to evaluation")
     parser.add_argument("--max_grad", default=1.0, type=float, help="Parameter for huber loss")
     parser.add_argument("--log_dir", default="log", type=str, help="specify log folder to save evaluate result")
@@ -52,8 +87,9 @@ def main():  # noqa: D103
     # memory related args
     parser.add_argument("--buffer_size", default=40000, type=int, help="reply memory buffer size")
     parser.add_argument("--look_back_steps", default=1, type=int, help="how many previous pricing tables will be fed into RL")
-    
+
     args = parser.parse_args()
+    args.output = get_output_folder(args.output, args.env)
     # print("\nParameters:")
     # for arg in vars(args):
     #     print(arg, getattr(args, arg))
@@ -61,12 +97,12 @@ def main():  # noqa: D103
     if os.path.exists(args.output):
         shutil.rmtree(args.output)
     os.mkdir(args.output)
-    
+
     if os.path.exists(args.log_dir):
         shutil.rmtree(args.log_dir)
     os.mkdir(args.log_dir)
     # Initiating policy for both tasks (training and evaluating)
-    policy = LinearDecayGreedyEpsilonPolicy(start_value=1, end_value=0.1, 
+    policy = LinearDecayGreedyEpsilonPolicy(start_value=1, end_value=0.1,
                                             num_steps=50000, num_actions=args.num_actions)
 
     if not args.train:
@@ -76,9 +112,6 @@ def main():  # noqa: D103
             print("Model path must be set when evaluate")
             exit(1)
 
-        # specific log file to save result
-        log_file = os.path.join(args.log_dir, args.network_name, str(args.model_num))
-        model_dir = os.path.join(args.model_path, args.network_name, str(args.model_num))
 
         with tf.Session() as sess:
             with tf.device('/cpu:0'):
@@ -95,16 +128,16 @@ def main():  # noqa: D103
             eval_driver_sim = Drivers()
             eval_env = Environment(driver_sim=eval_driver_sim)
             eval_memory = ReplayMemory(args.buffer_size, args.look_back_steps)
-            eval_policy = LinearDecayGreedyEpsilonPolicy(start_value=1, end_value=0.1, 
+            eval_policy = LinearDecayGreedyEpsilonPolicy(start_value=1, end_value=0.1,
                                             num_steps=1000000, num_actions=args.num_actions)
 
 
             q_network = create_model(args.look_back_steps, args.map_shape, args.num_actions)
-            dqn_agent = DQNAgent(q_network=q_network, memory=None, policy=policy, gamma=args.gamma, 
-                                target_update_freq=args.target_update_freq, num_burn_in=args.num_burn_in, 
+            dqn_agent = DQNAgent(q_network=q_network, memory=None, policy=policy, gamma=args.gamma,
+                                target_update_freq=args.target_update_freq, num_burn_in=args.num_burn_in,
                                 train_freq=args.train_freq, batch_size=args.batch_size, train_interv=args.train_interv,
                                 log_dir=args.log_dir)
-            
+
             dqn_agent.evaluate(eval_env, eval_memory, eval_policy, 10, 50)
         exit(0)
 
@@ -123,8 +156,8 @@ def main():  # noqa: D103
 
         memory = ReplayMemory(args.buffer_size, args.look_back_steps)
         q_network = create_model(args.look_back_steps, args.map_shape, args.num_actions)
-        dqn_agent = DQNAgent(q_network=q_network, memory=memory, policy=policy, gamma=args.gamma, 
-                                target_update_freq=args.target_update_freq, num_burn_in=args.num_burn_in, 
+        dqn_agent = DQNAgent(q_network=q_network, memory=memory, policy=policy, gamma=args.gamma,
+                                target_update_freq=args.target_update_freq, num_burn_in=args.num_burn_in,
                                 train_freq=args.train_freq, batch_size=args.batch_size, train_interv=args.train_interv,
                                 log_dir=args.log_dir)
         print( "defined dqn agent")
@@ -137,7 +170,7 @@ def main():  # noqa: D103
 
         lrs = LearningRateScheduler(lrdecay)
         '''
-        optimizer = SGD(lr=args.alpha) 
+        optimizer = SGD(lr=args.alpha)
         q_network.compile(optimizer, mean_huber_loss)
 
         sess.run(tf.global_variables_initializer())
@@ -150,12 +183,11 @@ def main():  # noqa: D103
         eval_driver_sim = Drivers()
         eval_env = Environment(driver_sim=eval_driver_sim)
         eval_memory = ReplayMemory(args.buffer_size, args.look_back_steps)
-        eval_policy = LinearDecayGreedyEpsilonPolicy(start_value=1, end_value=0.1, 
-                                            num_steps=1000000, num_actions=args.num_actions)
-        dqn_agent.fit(env=env, num_iterations=args.num_iterations,
+        eval_policy = LinearDecayGreedyEpsilonPolicy(start_value=1, end_value=0.1,
+                                            num_steps=100, num_actions=args.num_actions)
+        dqn_agent.fit(env=env, output_add=args.output,
                           eval_env=eval_env, eval_policy=eval_policy, eval_memory=eval_memory,
-                          output_dir=os.path.join(args.output),
-                          max_episode_length=args.max_episode_length, save_interval=args.save_interval)
+                          num_iterations=args.num_iterations, max_episode_length=args.max_episode_length)
 
 if __name__ == '__main__':
     main()
