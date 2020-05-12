81ad1a0184d76d97331d9704b6032fa651cd9f81
Suesoso <xssthu@gmail.com>
2020-05-12 11:36:41 -0700

    fix bugs

--- RL_main.py
+++ RL_main.py
@@ -19,6 +19,14 @@ from RL.objectives import mean_huber_loss
 from keras.optimizers import Adam, SGD
 from keras.callbacks import LearningRateScheduler
 
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 def get_output_folder(parent_dir, env_name):
     """Return save folder.
